38909d7755fe32be7af3b39296ed46b00f034e79
kutti128 <kutti128@gmail.com>
2020-05-06 15:04:44 -0500

    first commit

---
+++ src/java/javaclass.java
@@ -0,0 +1,11 @@
+package java;
+
+public class javaclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is a java program");
+	}
+
+}
